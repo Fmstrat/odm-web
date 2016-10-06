ab9330d96ba39e9eaf1ddca9d63b4e3a7efc3db8
Björn Tantau <github.com@s.bjoern-tantau.de>
2016-10-06 15:35:39 +0200

    Make PDO::MYSQL_ATTR_MAX_BUFFER_SIZE optional as it is not available for some versions of PHP.

--- odm/include/db.php
+++ odm/include/db.php
@@ -4,7 +4,11 @@
 
 	function dbconnect() {
 		global $DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE, $con;
-		$con = new PDO('mysql:dbname='.$DB_DATABASE.';host='.$DB_HOST.';charset=utf8', $DB_USER, $DB_PASSWORD, array(PDO::MYSQL_ATTR_MAX_BUFFER_SIZE=>1024*1024*50));
+		if (defined('PDO::MYSQL_ATTR_MAX_BUFFER_SIZE')) {
+			$con = new PDO('mysql:dbname='.$DB_DATABASE.';host='.$DB_HOST.';charset=utf8', $DB_USER, $DB_PASSWORD, array(PDO::MYSQL_ATTR_MAX_BUFFER_SIZE=>1024*1024*50));
+		} else {
+			$con = new PDO('mysql:dbname='.$DB_DATABASE.';host='.$DB_HOST.';charset=utf8', $DB_USER, $DB_PASSWORD);
+		}
 		$con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
